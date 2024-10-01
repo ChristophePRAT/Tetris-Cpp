2674c32757befa37a7ea20e825cbae3902f74550
Christophe Prat <christopheprat@icloud.com>
2024-10-01 10:41:56 +0200

    beginning MLP, WIP

--- AI/NN.cpp
+++ AI/NN.cpp
@@ -6,4 +6,37 @@
 //
 
 #include "NN.hpp"
+#include "mlx/array.h"
 #include "mlx/mlx.h"
+#include "mlx/ops.h"
+#include "mlx/array.h"
+using namespace mlx::core;
+
+array relu(const array& input) {
+  return maximum(input, 0.0); // Applies element-wise maximum [1]
+}
+
+double forward(mlp* ml, array input) {
+  // Layer 1
+  array z1 = matmul(input, ml->w1) + ml->b1; // Linear transformation [2]
+  array a1 = relu(z1);  // ReLU activation
+
+  // Layer 2
+  array z2 = matmul(a1, ml->w2) + ml->b2;
+  array a2 = relu(z2);
+
+  // Layer 3
+  array z3 = matmul(a2, ml->w3) + ml->b3;
+  array a3 = relu(z3);
+
+  // Output Layer
+  array z4 = matmul(a3, ml->w4) + ml->b4;
+  array output = (z4); // Assuming a sigmoid output
+
+  return output; // Assuming a single output value
+}
+
+
+void updateNN(mlp *ml, double loss) {
+
+}
--- AI/NN.hpp
+++ AI/NN.hpp
@@ -10,16 +10,30 @@
 
 #include <stdio.h>
 #include <mlx/mlx.h>
-class MLP {
-    public:
-        MLP(int nInput, int nHidden, int nOutput);
-        ~MLP();
-        void forward(mlx::core::array input);
-        void backward(mlx::core::array target);
-    private:
-        int nInput;
-        int nHidden;
-        int nOutput;
-};
+
+typedef struct MLP {
+    // Taille input = 6 pour le moment (hauteur max, hmax - hmin, delta h col, somme col, nb trous et nb de lignes effacées)
+    unsigned int input_dim = 6;
+    unsigned int couche_1 = 64;
+    unsigned int couche_2 = 64;
+    unsigned int couche_3 = 32;
+    unsigned int output_dim = 1;
+
+    mlx::core::array *w1;
+    mlx::core::array *b1;
+
+    mlx::core::array *w2;
+    mlx::core::array *b2;
+
+    mlx::core::array *w3;
+    mlx::core::array *b3;
+
+    mlx::core::array *w4;
+    mlx::core::array *b4;
+
+
+    // Taille output = 1 pour le moment (valeur de la fonction d'utilité)
+
+} mlp;
 
 #endif /* NN_hpp */
