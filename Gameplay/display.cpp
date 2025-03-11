f7f47daaf6cbae0cf991f864f6388d1d0201370e
Christophe Prat <christopheprat@icloud.com>
2025-03-11 11:56:21 +0100

    added shading

--- Gameplay/display.cpp
+++ Gameplay/display.cpp
@@ -4,7 +4,7 @@
 #include <SDL2_image/SDL_image.h>
 #include "SDL2/SDL_pixels.h"
 #include "SDL2/SDL_render.h"
-// #include "SDL2/SDL_video.h"
+#include <algorithm>
 #include "../Helpers/blocksNshapes.hpp"
 
 void renderText(SDL_Renderer *renderer, TTF_Font* Sans, int s, char* str, int y) {
@@ -60,12 +60,58 @@ void renderString(SDL_Renderer *renderer, TTF_Font* Sans, char* str, const char*
     SDL_DestroyTexture(Message);
 }
 
-void drawSquare(SDL_Rect* squareRect, SDL_Color* color, SDL_Renderer* renderer) {
-    // Set renderer color to draw the square
+void drawSquare(SDL_Rect* squareRect, SDL_Color* color, SDL_Renderer* renderer, bool withShading = true) {
+    // Set renderer color to draw the main square
     SDL_SetRenderDrawColor(renderer, color->r, color->g, color->b, color->a);
 
     // Draw filled square
     SDL_RenderFillRect(renderer, squareRect);
+
+    // If shading is disabled, return here
+    if (!withShading) {
+        return;
+    }
+
+    // Calculate border thickness (e.g., 2 pixels or 15% of square size)
+    int borderThickness = std::min(squareRect->w, squareRect->h) / 8;
+
+    // Create lighter shade for top/left borders
+    SDL_Color lightShade = {
+        (Uint8)std::min(255, color->r + 50),
+        (Uint8)std::min(255, color->g + 50),
+        (Uint8)std::min(255, color->b + 50),
+        color->a
+    };
+
+    // Create darker shade for bottom/right borders
+    SDL_Color darkShade = {
+        (Uint8)std::max(0, color->r - 50),
+        (Uint8)std::max(0, color->g - 50),
+        (Uint8)std::max(0, color->b - 50),
+        color->a
+    };
+
+    // Draw lighter top border
+    SDL_SetRenderDrawColor(renderer, lightShade.r, lightShade.g, lightShade.b, lightShade.a);
+    for(int i = 0; i < borderThickness; i++) {
+        SDL_RenderDrawLine(renderer,
+            squareRect->x + i, squareRect->y + i,
+            squareRect->x + squareRect->w - i, squareRect->y + i);
+        SDL_RenderDrawLine(renderer,
+            squareRect->x + i, squareRect->y + i,
+            squareRect->x + i, squareRect->y + squareRect->h - i);
+    }
+
+    // Draw darker bottom border
+    SDL_SetRenderDrawColor(renderer, darkShade.r, darkShade.g, darkShade.b, darkShade.a);
+    for(int i = 0; i < borderThickness; i++) {
+        SDL_RenderDrawLine(renderer,
+            squareRect->x + i, squareRect->y + squareRect->h - i,
+            squareRect->x + squareRect->w - i, squareRect->y + squareRect->h - i);
+        SDL_RenderDrawLine(renderer,
+            squareRect->x + squareRect->w - i, squareRect->y + i,
+            squareRect->x + squareRect->w - i, squareRect->y + squareRect->h - i);
+    }
 }
 
 void drawBlock(block s, int x, int y, SDL_Renderer* renderer) {
@@ -79,7 +125,7 @@ void drawBlock(block s, int x, int y, SDL_Renderer* renderer) {
             squareRect.h = SQUARE_WIDTH;
             if (s.shape[s.currentShape][i][j] != 0) {
                 color = blockColors[s.shape[s.currentShape][i][j]];
-                drawSquare(&squareRect, &color, renderer);
+                drawSquare(&squareRect, &color, renderer, true);
             }
         }
     }
@@ -95,8 +141,8 @@ void drawMat(mat m, block s, SDL_Renderer* renderer, int TIMER_INTERVAL, int x, 
     squareRect.w = SQUARE_WIDTH * (m.cols + x);
     squareRect.h = SQUARE_WIDTH * (m.rows + y);
     squareRect.x = SQUARE_WIDTH;
-    squareRect.y = SQUARE_WIDTH;
-    drawSquare(&squareRect, &color, renderer);
+    squareRect.y = SQUARE_WIDTH + 1;
+    drawSquare(&squareRect, &color, renderer, false);
 
     for (int i = 0; i < m.rows; i++) {
         squareRect.y = (i + 1) * SQUARE_WIDTH;
@@ -106,19 +152,15 @@ void drawMat(mat m, block s, SDL_Renderer* renderer, int TIMER_INTERVAL, int x, 
             squareRect.h = SQUARE_WIDTH;
             if (m.data[i][j] != 0) {
                 color = blockColors[m.data[i][j]];
-                drawSquare(&squareRect, &color, renderer);
+                drawSquare(&squareRect, &color, renderer, true);
             } else {
                 i_offset = i - s.position[0];
                 j_offset = j - s.position[1];
                 if (TIMER_INTERVAL >= 20 && i_offset >= 0 && j_offset >= 0 && i_offset < 4 && j_offset < 4 &&
                     s.shape[s.currentShape][i_offset][j_offset] != 0) {
                     color = blockColors[s.shape[s.currentShape][i_offset][j_offset]];
-                    drawSquare(&squareRect, &color, renderer);
+                    drawSquare(&squareRect, &color, renderer, true);
                 }
-                // else {
-                //     color = blockColors[0];
-                //     drawSquare(&squareRect, &color, renderer);
-                // }
             }
         }
     }
